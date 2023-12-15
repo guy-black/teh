dea6684e3960f0eb7c31fe763105230045cac832
guy <guyb2602@gmail.com>
2023-12-14 19:54:20 -0600

    no meds just manic desire to finish this
    implemented -c and -ic flags
    almost done with new smoother main function
    gonna delete so much messy code after it's ready
    also learned that readFile doesn't work well with non plaintext files

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,38 @@ module Main (main) where
 import System.Environment (getArgs)
 import System.Directory (doesFileExist, XdgDirectory(XdgConfig), getXdgDirectory)
 import Text.Read (readMaybe)
-import qualified Data.Map as M    -- for M.Map
-import Data.Map ((!?))
-import Data.List (isSuffixOf, sortOn)
+import qualified Data.Map.Strict as M    -- for M.Map
+import Data.Map.Strict ((!?))
+import Data.List (isSuffixOf, sortOn, (\\), nub)
 import Data.Either (partitionEithers)
 import Data.Char (isSpace, isDigit)
 import qualified Data.Text as T
 import GHC.IO.StdHandles (stderr)
 import GHC.IO.Handle (hPutStr)
 
+
 main :: IO ()
 main = do
   args <- getArgs >>= (return . (map T.pack)) -- get args passed in
-  confDir <- getXdgDirectory XdgConfig ".teh" >>= (return . T.pack) -- get path for where the user .teh conffile would be if it exists
+  if (any (`elem`args) ["-h", "--help"]) || null args then -- checking for an early exit condition
+    putTxt help
+  else if args == ["--penguin"] then do  -- hehe we have fun here :)
+    putTxt pod
+  else do  -- no early exit case, start compiling list of macro files
+    confDir <- getXdgDirectory XdgConfig ".teh" >>= (return . T.pack) -- get path for where the user .teh file should be
+    let (cflagmacs, argsminuscflag) = extArgs (=="-c") args
+    let (macstoignore, maclessargs) = extArgs (=="-ic") argsminuscflag
+    let finalmaclist = (dedupe $ confDir:".teh":cflagmacs) `remAll` macstoignore -- final list of files to look for macros
+    parsedMacsAndErrs <- mapM seekMacs finalmaclist -- convert our files to look for macros into a [(errors::T.Text, Macros)]
+    if "--show-macros" `elem` maclessargs then -- we don't actually need to edit text, just parse macros and print details
+      putTxt $ printMacros parsedMacsAndErrs -- TODO actually make printMacros do something useful
+    else do  -- we can squish our parsed marcros into one big Macros and toss forget errors or which file they came from
+      let finalMacs = mconcat $ reverse $ map snd parsedMacsAndErrs
+      -- mconcat will favor values to the left, but I want to favor values to the right, so reverse it first
+      return ()
+
+
+{--
   conmac@(conerr, conmacs) <- seekMacs confDir -- Tuple of T.Text and Map T.Text [Change] if it was read with no problem string will
   locmac@(locerr, locmacs) <- seekMacs ".teh" -- be empty, if no file or parse error string will say that with empty map
   let (macErrors, combMacs) = combineMacros conmac locmac
@@ -32,6 +51,7 @@ main = do
       putTxt $ teh edits stdin
     WoStdIn (edits, txt) ->
       putTxt $ teh edits txt
+--}
 
 seekMacs :: T.Text ->  IO (T.Text, Macros)
 seekMacs f = do
@@ -115,77 +135,12 @@ doChanges chg txt = -- applying change to whole blob of text
       doChanges chgs (T.replace find repl txt)
 
 
-type Edit  = (Target, [Change])
-
-data Change = Fr T.Text T.Text
-            | Ins T.Text Int
-            | Rem Int Int
-  deriving (Read, Show)
-
-data Target = Whole
-            | Each
-            | Only [Int]
-  deriving (Read, Show)
-
-type Macros = M.Map T.Text Edit
-
-
-
-
-(<+>) :: [a] -> a -> [a]
-(<+>) xs x = xs<>[x]
-
-pod :: T.Text
-pod =
- "hi every1 im new!!!!!!! *holds up spork* my name is katy but u can call me t3h PeNgU1N oF d00m!!!!!!!! lol…as u can see im very random!!!! thats why i came here, 2 meet random ppl like me ^_^… im 13 years old (im mature 4 my age tho!!) i like 2 watch invader zim w/ my girlfreind (im bi if u dont like it deal w/it) its our favorite tv show!!! bcuz its SOOOO random!!!! shes random 2 of course but i want 2 meet more random ppl =) like they say the more the merrier!!!! lol…neways i hope 2 make alot of freinds here so give me lots of commentses!!!!\n\
-\DOOOOOMMMM!!!!!!!!!!!!!!!! <--- me bein random again ^_^ hehe…toodles!!!!!\n\
-\\n\
-\love and waffles,\n\
-\\n\
-\t3h PeNgU1N oF d00m"
-
-help :: T.Text
-help =
-  "Usage: teh [options]\n\
-  \   by default teh reads from StdIn for text to apply changes to.  To disable this either pass the --nostdin flag or\n\
-  \   pass a filename as the final argument \
-  \   -h will bring you this menu and --listmacros will list all macros found in the config folder and in the local directory\
-  \   macros can be written in the .teh file in a directory or in your xdgconfig directory\
-  \   commands are written in the form of 'Ch <Whole, Each, Only [1, 2, 3]> (<Fr \"find\" \"replce\", Rem 0 1, Ins 0 \"insert\">)'\
-  \   See README for more details "
-
 -- takes the config file then the local file
-combineMacros :: (T.Text, Macros) -> (T.Text, Macros) -> (T.Text, Macros)
-combineMacros (cErr, cMac) (lErr,lMac) =
-  (cErr<>lErr, lMac<>cMac) -- <> for Map favors left if both have same key and local overrides conf so local on left
+combineMacros :: [Macros] -> Macros
+combineMacros = mconcat . reverse -- <> for Map favors left if both have same key and local overrides conf so local on left
 
 
-takeEnd :: Int -> T.Text -> T.Text
-takeEnd n  = (T.reverse . T.take n . T.reverse)
-
-dropEnd :: Int -> T.Text -> T.Text
-dropEnd n  = (T.reverse . T.drop n . T.reverse)
-
-droplen :: T.Text -> T.Text -> T.Text
-droplen stub whole = T.drop (T.length stub) whole
-
-takeLast :: Int -> [a] -> [a]
-takeLast n = (reverse . take n . reverse)
-
-dropLast :: Int -> [a] -> [a]
-dropLast n = (reverse . drop n . reverse)
-
-mapIf :: (a -> a) -> (a -> Bool) -> [a] -> [a]
-mapIf _ _ [] = []
-mapIf f b (x:xs) =
-  if b x then
-    (f x):(mapIf f b xs)
-  else
-    x : (mapIf f b xs)
-
-putStdErr :: T.Text -> IO()
-putStdErr = hPutStr stderr . T.unpack . (<>"\n")
-
+{-- all of this funcitonality should be moved into main I think I'll be fine removing it all together
 howToProceed :: Macros -> [T.Text] -> IO Proceed
 howToProceed mac sts =
   case sts of
@@ -216,17 +171,9 @@ howToProceed mac sts =
               case parseargs mac sts of
                 Left err -> return $ Stop $ ArgParseErr err
                 Right edits -> return $ WithStdIn edits
+--}
 
-data Proceed = WithStdIn [Edit]
-             | WoStdIn ([Edit], T.Text)
-             | Stop Cause
-
-data Cause = Help
-           | Macs
-           | Penguin
-           | ArgParseErr T.Text
-
-printMacros :: (T.Text, Macros) -> T.Text
+printMacros :: [(T.Text, Macros)] -> T.Text
 printMacros _ = "I'll do it this afternooooon"
 -- eventually this will be replaced with a funtion that will generate a pretty piece of text to show all in scope macros and any parse errors
 -- for now it just kind of sorta technically works if you squint a bit and don't enjoy life
@@ -237,23 +184,6 @@ printMacros _ = "I'll do it this afternooooon"
 --   takeUntil b ls == ([], ls)
 -- if some value a within ls is True for b then for the first a
 --   takeuntil b ls == (prea<+>a,posta)
-takeUntil ::  (a -> Bool) -> [a] -> ([a],[a])
-takeUntil b ls =
-  case break b ls of
-    (_, []) -> ([], ls)
-    (pre, (r:st)) -> (pre<+>r,st)
-
-showT :: Show a => a -> T.Text
-showT = T.pack . show
-
-readMaybeT :: Read a => T.Text -> Maybe a
-readMaybeT = readMaybe . T.unpack
-
-readT :: Read a => T.Text ->  a
-readT = read . T.unpack
-
-putTxt :: T.Text -> IO ()
-putTxt = putStr . T.unpack
 
 parseargs :: Macros -> [T.Text] -> Either T.Text [Edit]
 parseargs macs ts = parseargs' macs ts []
@@ -348,6 +278,38 @@ parseChgs' m (t:ts) acc = -- undefined
   else
     Left $ "Error: " <> (T.unwords $ t:ts) <> " was expected to be a Change, but changes must start with Fr, Rem, or Ins.  Run with -h for more info"
 
+
+-- -----------*
+--  datatypes
+-- -----------*
+
+data Proceed = WithStdIn [Edit]
+             | WoStdIn ([Edit], T.Text)
+             | Stop Cause
+
+data Cause = Help
+           | Macs
+           | Penguin
+           | ArgParseErr T.Text
+
+type Edit  = (Target, [Change])
+
+data Change = Fr T.Text T.Text
+            | Ins T.Text Int
+            | Rem Int Int
+  deriving (Read, Show)
+
+data Target = Whole
+            | Each
+            | Only [Int]
+  deriving (Read, Show)
+
+type Macros = M.Map T.Text Edit
+
+-- -----------------*
+-- helper functions
+-- -----------------*
+
 -- like T.words, but concatenates a word that start's with '"' with all subsequent words until found a word that ends
 -- with '"' only if it's not preceded by a '\'
 -- TODO: ignore closing escaped quote mark ending a quote
@@ -364,11 +326,108 @@ wordsandquo t =
           T.words t
         (quo, aftquo) -> pre <> [(T.unwords quo)] <> (wordsandquo $ T.unwords aftquo)
 
-tmptxt :: T.Text
-tmptxt = "this is a Text value with no quotes"
+takeUntil ::  (a -> Bool) -> [a] -> ([a],[a])
+takeUntil b ls =
+  case break b ls of
+    (_, []) -> ([], ls)
+    (pre, (r:st)) -> (pre<+>r,st)
 
-tmptxt2 :: T.Text
-tmptxt2 = "this is a \"Text typed value\" with \"no quotes\" ;)"
+-- split a list into the values after a value that returns true and the rest of the list without the true values and their successor
+-- or more concretely, will be used to pull out the arguments that directly follow a given flag and the rest of the args
+extArgs :: (a -> Bool) -> [a] -> ([a], [a])
+extArgs f ls =
+  case ls of
+    [] -> ([],[]) -- an empty list, just double it
+    [a] -> ([],[a]) -- again can't do anything with a singleton list
+    _ -> extArgs' f ([], []) ls -- this list has two or more values so now we can call the real function
 
-tmptxt3 :: T.Text
-tmptxt3 = "this is a text value with a \"quote that never closes oh nooooo"
+{-- TODO: figure out why defining extArgs like this makes ghc unhappy
+    "Multiple declarations of 'extArg', but it only pointed out the first and the third one, not the second
+    this was before it started complaining that I was calling it as extArgs above
+extArg _ [] = ([],[]) -- an empty list, just double it
+estArg _ (a:[]) = ([],[a]) -- again can't do anything with a singleton list
+extArg f ls@(a:aa) = extArg' f ([], []) ls -- this list has two or more values so now we can call the real function
+--}
+
+extArgs' :: (a -> Bool) -> ([a], [a]) -> [a] -> ([a], [a]) -- the less ergonomic function we actually need
+extArgs' _ (wnt, othr) [] = (wnt, othr) -- empty list to sort through, base case.  I remember when recursion was hard to understand
+extArgs' _ (wnt, othr) [a] = (wnt, othr<+>a) -- one item list, other base case.  Now I just slap this together without even thinking
+extArgs' f (wnt, othr) (h:hh:rem) =
+  if f h then -- checking if we want to add to the wanted accumulator
+    extArgs' f (wnt<+>hh, othr) rem -- we found what we want so we drop the flag and the argument and recurse with what's left
+  else
+    extArgs' f (wnt, othr<+>h) (hh:rem) -- not a flag we wanted, add it to the others and recurse
+
+showT :: Show a => a -> T.Text
+showT = T.pack . show
+
+readMaybeT :: Read a => T.Text -> Maybe a
+readMaybeT = readMaybe . T.unpack
+
+readT :: Read a => T.Text ->  a
+readT = read . T.unpack
+
+putTxt :: T.Text -> IO ()
+putTxt = putStr . T.unpack
+
+takeEnd :: Int -> T.Text -> T.Text
+takeEnd n  = (T.reverse . T.take n . T.reverse)
+
+dropEnd :: Int -> T.Text -> T.Text
+dropEnd n  = (T.reverse . T.drop n . T.reverse)
+
+droplen :: T.Text -> T.Text -> T.Text
+droplen stub whole = T.drop (T.length stub) whole
+
+takeLast :: Int -> [a] -> [a]
+takeLast n = (reverse . take n . reverse)
+
+dropLast :: Int -> [a] -> [a]
+dropLast n = (reverse . drop n . reverse)
+
+-- remove any duplicate values from a list, preserving order, leaving last iteration of an item in the list
+-- nub typically only saves first instance of a value and removing remaing instances
+-- I want to save the last instance of each value and remove their predecessors and retain order, so wrap it in reverses
+dedupe :: Eq a => [a] -> [a]
+dedupe = reverse . nub . reverse -- using reverse twice feels icky so maybe time this with a custom recursive version later
+
+-- remove every item from first list that is in second list
+remAll :: Eq a => [a] -> [a] -> [a]
+remAll ls [] = ls -- base case, no more values to remove
+remAll ls (h:t) = remAll (filter (/=h) ls) (filter (/=h) t) -- recurse filtering out all values of head from both lists
+
+-- a conditional map
+-- only works with a function that evaluates to a value of the same type as it's input
+-- it is only applied to values of a list that satisfy a function that returns bool
+mapIf :: (a -> a) -> (a -> Bool) -> [a] -> [a]
+mapIf _ _ [] = []
+mapIf f b (x:xs) =
+  if b x then
+    (f x):(mapIf f b xs)
+  else
+    x : (mapIf f b xs)
+
+putStdErr :: T.Text -> IO()
+putStdErr = hPutStr stderr . T.unpack . (<>"\n")
+
+(<+>) :: [a] -> a -> [a]
+(<+>) xs x = xs<>[x]
+
+pod :: T.Text
+pod =
+ "hi every1 im new!!!!!!! *holds up spork* my name is katy but u can call me t3h PeNgU1N oF d00m!!!!!!!! lol…as u can see im very random!!!! thats why i came here, 2 meet random ppl like me ^_^… im 13 years old (im mature 4 my age tho!!) i like 2 watch invader zim w/ my girlfreind (im bi if u dont like it deal w/it) its our favorite tv show!!! bcuz its SOOOO random!!!! shes random 2 of course but i want 2 meet more random ppl =) like they say the more the merrier!!!! lol…neways i hope 2 make alot of freinds here so give me lots of commentses!!!!\n\
+\DOOOOOMMMM!!!!!!!!!!!!!!!! <--- me bein random again ^_^ hehe…toodles!!!!!\n\
+\\n\
+\love and waffles,\n\
+\\n\
+\t3h PeNgU1N oF d00m"
+
+help :: T.Text
+help =
+  "Usage: teh [options]\n\
+  \   by default teh reads from StdIn for text to apply changes to.  To disable this either pass the --nostdin flag or\n\
+  \   pass a filename as the final argument \
+  \   -h will bring you this menu and --listmacros will list all macros found in the config folder and in the local directory\
+  \   macros can be written in the .teh file in a directory or in your xdgconfig directory\
+  \   commands are written in the form of 'Ch <Whole, Each, Only [1, 2, 3]> (<Fr \"find\" \"replce\", Rem 0 1, Ins 0 \"insert\">)'\
+  \   See README for more details "
